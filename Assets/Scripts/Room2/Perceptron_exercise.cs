42c3a19b822dc2cfcbdea4c1d57aa30b9f4a7656
rsb0 <runesb@stud.ntnu.no>
2020-10-17 16:39:52 +0200

    added information board about sigmoid neuron

---
+++ Assets/Scripts/Room2/Perceptron_exercise.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Perceptron_exercise : MonoBehaviour
+{
+    public GameObject outputPlaceholder;
+    public TextMeshProUGUI inputOneText;
+    public TextMeshProUGUI inputTwoText;
+    public TextMeshProUGUI outputText;
+    public TextMeshProUGUI neuronText;
+    public Material outputRightAswerMaterial;
+    public Material outputWrongAswerMaterial;
+
+    private int inputOne;
+    private int inputTwo;
+    private int Result;
+
+    private static Color32 WHITE = new Color32(255, 255, 255, 255);
+    private static Color32 BLACK = new Color32(0, 0, 0, 255);
+
+    private void Awake()
+    {
+        this.inputOne = 0;
+        this.inputTwo = 0;
+        this.Result = 0;
+    }
+
+    public void UpdateInputOne(int input)
+    {
+        this.inputOne = input;
+        inputOneText.text = "x1 = " + input.ToString();
+    }
+
+    public void UpdateInputTwo(int input)
+    {
+        this.inputTwo = input;
+        inputTwoText.text = "x2 = " + input.ToString();
+    }
+
+    public void UpdatePerceptron()
+    {
+        this.Result = inputOne * 2 + inputTwo;
+        neuronText.text = this.inputOne.ToString() + "x2+" + this.inputTwo.ToString() + "x1";
+
+        
+        if (Result >= 6)
+        {
+            outputPlaceholder.GetComponent<MeshRenderer>().material = outputRightAswerMaterial;
+            outputText.text = "y = 1";
+            outputText.color = BLACK;
+        }
+        else
+        {
+            outputPlaceholder.GetComponent<MeshRenderer>().material = outputWrongAswerMaterial;
+            outputText.color = WHITE;
+            outputText.text = "y = 0";
+        }
+    }
+}
---
+++ Assets/Scripts/SetActiveScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Remoting;
+using UnityEngine;
+using Zinnia.Action;
+
+public class SetActiveScript : MonoBehaviour
+{
+    public GameObject myObject;
+    public bool activate;
+
+    public BooleanAction ActivationAction;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activate)
+        {
+            myObject.SetActive(true);
+        }
+        else
+        {
+            myObject.SetActive(false);
+        }
+    }
+}
